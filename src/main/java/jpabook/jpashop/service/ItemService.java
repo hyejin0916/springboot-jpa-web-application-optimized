1e4e45c82d3820c634c05057c631ac8bc4493763
hyejin0916 <rkdgpwlsz@naver.com>
2025-12-28 17:25:12 +0900

    feat: section6-21 implement item service

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 상품 저장
+     */
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    /**
+     * 모든 상품 조회
+     */
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    /**
+     * 단일 상품 조회
+     */
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
