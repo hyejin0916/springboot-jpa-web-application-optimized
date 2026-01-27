ac2106383e2a1fff050613472d4b807f7fc406bd
hyejin0916 <rkdgpwlsz@naver.com>
2026-01-27 17:53:31 +0900

    feat: section5-17 add order retrieval API v3.1 (OneToMany, XToOne fetch join + BatchSize for paging fix)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -55,11 +56,28 @@ public class OrderApiController {
 
     /**
      * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
-     * hibernate 6.x부터 distinct 자동 적용 -> 데이터 중복 현상 해결, 페이징 문제는 해결 X
+     * - hibernate 6.x부터 distinct 자동 적용 -> 데이터 중복 현상 해결, 페이징 문제는 해결 X
      */
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
